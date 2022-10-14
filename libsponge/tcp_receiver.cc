3eae317dfe2ea2c8d1e999ce55a9331ad2364e5b
xjh <xing_jiahao@qq.com>
2022-10-14 03:33:09 +0000

    lab2 finished

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -5,15 +5,41 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+// template <typename... Targs>
+// void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    if(seg.header().syn){
+        _is_setISN=true;
+        _ISN=seg.header().seqno;
+    }
+    else{
+        if(_is_setISN==false) return;
+    }
+    uint64_t index=unwrap(seg.header().seqno,_ISN,_reassembler.stream_out().bytes_written());
+    string s=seg.payload().copy();
+    if(seg.header().syn){
+        _reassembler.push_substring(s,index,seg.header().fin);
+    }
+    else{
+        _reassembler.push_substring(s,index-1,seg.header().fin);
+    }
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if(_is_setISN){
+        if(_reassembler.stream_out().input_ended()) 
+            return wrap(_reassembler.stream_out().bytes_written()+2,_ISN);
+        else
+            return wrap(_reassembler.stream_out().bytes_written()+1,_ISN);
+    }
+    return nullopt;
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const {
+    return  _capacity
+            -_reassembler.stream_out().bytes_written()
+            +_reassembler.stream_out().bytes_read();
+}
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -5,8 +5,8 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+// template <typename... Targs>
+// void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t r=n%4294967296 +isn.raw_value();
+    return WrappingInt32(r);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,29 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t t;
+    if(n.raw_value()>=isn.raw_value()){
+        t=n.raw_value()-isn.raw_value();
+    }
+    else{
+        t=n.raw_value()+(4294967296-isn.raw_value());
+    }
+    uint64_t pow=checkpoint / 4294967296;
+    uint64_t end=checkpoint % 4294967296;
+    if(max(end,t)-min(end,t)>=(4294967296/2)){
+        if(end>t){
+            if(pow<4294967295){
+                t=4294967296*(1+pow)+t;
+            }
+        }
+        else{
+            if(pow>0){
+                t=4294967296*(pow-1)+t;
+            }
+        }
+    }
+    else{
+        t=4294967296*pow+t;
+    }
+    return t;
 }
